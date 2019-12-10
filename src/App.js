0a6a33ea344fdffde380ece41876311cd9a3c216
rim-code <friouirim@gmail.com>
2019-12-10 14:10:21 +0100

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      seconds:0 , 
+      isOn:false
+     }
+  }
+start =()=> {
+  this.interval= setInterval(()=> {
+    this.setState({
+      seconds:this.state.seconds+1
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    })
+  },1000)
+  this.setState ({
+    isOn:true
+  })
 }
 
+Rest =()=> {
+  clearInterval(this.interval)
+  this.setState ({
+    seconds:0
+  })
+}
+Stop =()=> {
+  clearInterval(this.interval)
+  this.setState ({
+    isOn:false
+  })
+}
+counter(num) {
+  var a =num/3600
+
+
+  var h = Math.floor(a)
+  var m= Math.floor((a-h)*60)
+  var s= Math.round((((a-h)*60)-m)*60)
+ return (
+  <div> 
+  <span>{String(h).padStart(2,0)} : {String(m).padStart(2,0)} : {String(s).padStart(2,0)} </span>
+</div>
+ )
+
+     }
+  render() { 
+    return ( <div>
+  <span>{this.counter(this.state.seconds)}</span>
+
+
+<div>
+  <span>Hours</span>
+  <span>Minutes</span>
+  <span>seconds</span>
+
+</div>
+<div>
+  <button onClick ={!this.state.isOn ? this.start:this.Stop}>Start</button>
+  <button onClick={this.Rest}>Rest</button>
+
+</div>
+    </div> );
+  }
+}
+ 
 export default App;
